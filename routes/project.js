b80dd335043bb48f8e7a1a751c5de5b286af4975
Andrew Nguyen <andrewdnguyen64@gmail.com>
2018-02-15 10:52:19 -0800

    AJAX

--- routes/project.js
+++ routes/project.js
@@ -2,12 +2,20 @@ var projects = require('../projects.json');
 
 exports.projectInfo = function(req, res) { 
 	var projectID = req.params.id;
+	console.log(projectID);
 	if (projectID == "random") {
 		projectID = Math.floor(Math.random() * projects.length) + 1;
 	} else {
 		projectID = parseInt(projectID);
 	}
-
   	var project = projects[projectID-1]; // of by one, our first project has index 0
   	res.json(project);
+
+  	$(".details").click(loadup);
+}
+
+function loadup() {
+	if ($(this.id) == projectID){
+		$(this).html()
+	}
 }
